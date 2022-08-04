eca3630660d32bfa6ff1a452e3c7cf0a29d0a006
santhoshsurya32 <santhoshsurya321@gmail.com>
2022-08-05 00:36:08 +0530

    added more operators

---
+++ operator.js
@@ -0,0 +1,31 @@
+console.log(10 + 5);
+console.log(10 - 5);
+console.log(10 * 5);
+console.log(12 / 5); //result is in float
+console.log(10 % 2);
+console.log(10 ** 2); //power
+
+//assignment
+let a = 20;
+console.log(a + 10);
+let b = 5;
+b += 3;
+console.log(b);
+a **= 2;
+console.log(a);
+
+///comparitive
+
+let x = 20;
+let y = "20";
+console.log(x == y); //checks value but not datatype...
+console.log(x === y); ///checks both value and datatype...
+console.log(x != y);
+console.log(x !== y);
+
+///terinary
+
+5 > 10 ? console.log("hi") : console.log("hello");
+
+let l = "sam";
+let m = "and";
